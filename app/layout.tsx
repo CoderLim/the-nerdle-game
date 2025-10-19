dd37984f58f04a240f6bead9c3aa43ecdf8f43db
CoderLim <779793115@qq.com>
2025-10-19 23:49:46 +0800

    feat: plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          defer
+          data-domain="thenerdlegame.com"
+          src="https://app.pageview.app/js/script.js"
+          strategy="afterInteractive"
+        />
         {children}
       </body>
     </html>
