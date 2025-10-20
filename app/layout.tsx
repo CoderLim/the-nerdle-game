3ece44212eef9f73169fee8170d2d636cc72a68c
Lim <779793115@qq.com>
2025-10-20 13:08:49 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,19 @@ export default function RootLayout({
         />
         {children}
       </body>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-10HRFHRBVD"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-10HRFHRBVD');
+        `}
+      </Script>
     </html>
   );
 }
